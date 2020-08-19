7a3c8208b5fef237e5ad995b85bf2345135a9738
mohamed82008 <mohamed82008@gmail.com>
2020-08-19 18:17:21 +1000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.8

---
+++ src/ConvergencePlots.jl
@@ -0,0 +1,5 @@
+module ConvergencePlots
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ConvergencePlots
+using Test
+
+@testset "ConvergencePlots.jl" begin
+    # Write your tests here.
+end
